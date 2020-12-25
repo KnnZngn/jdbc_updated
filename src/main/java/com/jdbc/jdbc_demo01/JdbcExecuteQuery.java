179acefbfdc86a1f8cd6064629fcbec76a85d8be
apple <apple@apples-imac-6.attlocal.net>
2020-12-25 09:55:01 -0500

    fourth commit

--- src/main/java/com/jdbc/jdbc_demo01/JdbcExecuteQuery.java
+++ src/main/java/com/jdbc/jdbc_demo01/JdbcExecuteQuery.java
@@ -19,7 +19,10 @@ public class JdbcExecuteQuery {
 		// 4)Create the Statement ==> I started to talk
 		Statement st = con.createStatement();
 
-		// 1st Example
+				// 1st Example
+		//executeQuery(): This method is used to execute statements that returns tabular data (example select). 
+		//It returns an object of the class ResultSet.
+		
 		//5.Step: Execute the query and store the result ==> Get the sentences into your brain
 		ResultSet rs1 = st.executeQuery("SELECT * FROM workers");
 
@@ -28,7 +31,7 @@ public class JdbcExecuteQuery {
 			System.out.println(rs1.getString(1) + " - " + rs1.getString(2) + " - " + rs1.getInt(3));
 		}
 
-		// 2nd Example
+				// 2nd Example
 		//5.Step: Execute the query and store the result ==> Get the sentences into your brain
 		ResultSet rs2 = st.executeQuery("SELECT country_name FROM countries WHERE region_id > 2");
 
@@ -37,16 +40,24 @@ public class JdbcExecuteQuery {
 			System.out.println(rs2.getString(1));
 		}
 
-		// 3rd Example
+				// 3rd Example
 		//5.Step: Execute the query and store the result ==> Get the sentences into your brain
-		ResultSet rs3 = st.executeQuery("CREATE TABLE workers2(id char(2), name VARCHAR2(50))");
+		//execute() method: This method is used to execute SQL DDL statements, 
+		//it returns a boolean value specifying weather the ResultSet object can be retrieved.
+		boolean rs3 = st.execute("CREATE TABLE workers2(id char(2), name VARCHAR2(50))");
+		System.out.println(rs3);
+		
+				//4th Example
+		//executeUpdate(): This method is used to execute statements such as insert, update, delete. 
+		//It returns an integer value representing the number of rows affected.
+		int rs4 = st.executeUpdate("UPDATE workers SET salary = salary + 300 WHERE id > 10001");
+		System.out.println(rs4);
 
 		//7.Step: Close everything
 		con.close();
 		st.close();
 		rs1.close();
 		rs2.close();
-		rs3.close();
 
 	}
 }
